997cce9d69bb3568b021a7352f8b1ae09aad2d3d
Jaime David Guevara Rojas <jaimeguevara@u.boisestate.edu>
2019-10-02 21:12:59 -0600

    Adding more to the ignore pile

--- webgl_A2/a2.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var canvas = document.getElementById('hellowebgl');
-
-    canvas.addEventListener("click", hello);
-});
-
-function hello() {
-    console.log("hello");
-}
